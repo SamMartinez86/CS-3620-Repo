8167b49d7a77e8f7d5c15ff7b719deb18da978e5
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-01-26 20:56:41 -0700

    Update 2

--- assignment2.php
+++ assignment2.php
@@ -2,4 +2,4 @@
 header("Access-Control-Allow-Origin: *");
 ?>
 
-{"books-read":[{"Title":"Atlas Shrugged","Author":"Ayn Rand"},{"Title":"1984","Author":"George Orwell"},{"Title":"The Fountainhead","Author":"Ayn Rand"},{"Title":"Permanent Record","Author":"Edward Snowden"},]}
+{"books-read":[{"Title":"Atlas Shrugged","Author":"Ayn Rand"},{"Title":"1984","Author":"George Orwell"},{"Title":"Permanent Record","Author":"Edward Snowden"},]}
