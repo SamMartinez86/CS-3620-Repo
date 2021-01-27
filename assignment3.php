6dd34c841f21e213793d0252e42f1f3b34c53a8b
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-01-26 21:29:33 -0700

    Password problem?
    Connection failed: php_network_getaddresses: getaddrinfo failed: No such host is known.

--- assignment3.php
+++ assignment3.php
@@ -2,7 +2,7 @@
 // open db connection
 $servername = "https://cs3620app.azurewebsites.net";
 $username = "sam@cs3620sqlsam";
-$password = "1801church";
+$password = "1801Church!";
 $dbname = "cs3620schema";
 
 // Create connection
