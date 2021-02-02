0f29a0d51d43f2e8cca0d606197b691f857af5ff
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-02-02 13:52:59 -0700

    env

--- assignment3.php
+++ assignment3.php
@@ -5,11 +5,11 @@
 // open db connection
 $servername = "cs3620sqlsam.mysql.database.azure.com";
 
-$username = "sam@cs3620sqlsam";
-$password = "1801Church";
+//$username = "sam@cs3620sqlsam";
+//$password = "1801Church";
 
-//$username = (isset($_SESSION['SQLUSER'] ? $_SESSION ['SQLUSER'] : $_ENV['SQLUSER'];
-//$password = (isset($_SESSION['SQLPW'] ? $_SESSION ['SQLPW'] : $_ENV['SQLPW'];
+$username = (isset($_SESSION['SQLUSER'] ? $_SESSION ['SQLUSER'] : $_ENV['SQLUSER'];
+$password = (isset($_SESSION['SQLPW'] ? $_SESSION ['SQLPW'] : $_ENV['SQLPW'];
 $dbname = "cs3620schema";
 
 // Create connection
@@ -20,27 +20,9 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// query table
-$sql = "SELECT * FROM Food";
-// store query in variable
-$result = $conn->query($sql);
-
-// If rows exist in result
-if ($result->num_rows > 0) 
-{
-    while($row = $result->fetch_assoc()) // put results in array and loop
-    {
-        echo "Food Name: " . $row["FoodName"]. "Food Cost: " . $row["FoodCost"]. "<br>";
-    }
-
-} else 
-{
-    echo "No results, something is wrong."
-}
-
 // insert statement
-//$sql = "INSERT INTO Food (FoodName, FoodCost)
-//VALUES ('Lasagna', 8)";
+$sql = "INSERT INTO Food (FoodName, FoodCost)
+VALUES ('Lasagna', 8)";
 
 //Display records
 //$sql = "SELECT * FROM Food";
@@ -48,7 +30,7 @@ if ($result->num_rows > 0)
 
 // record confirmation 
 if ($conn->query($sql) === TRUE) {
-  echo "Displayed";
+  echo "New query inserted";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
