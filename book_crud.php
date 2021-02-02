6918cbdf36b79877061fedbc1be4f85bb2c402b3
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-02-02 14:47:52 -0700

    Notify single record

--- book_crud.php
+++ book_crud.php
@@ -59,10 +59,8 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
-  while($row = $result->fetch_assoc()) {
-    //echo "*** New Record *** <br>"
-    echo "Food Name: " . $row["FoodName"]. " Food Cost: " . $row["FoodCost"]. "<br>";
-    //echo "***            *** <br>"
+  while($row = $result->fetch_assoc()) {   
+    echo "(Single Record) Food Name: " . $row["FoodName"]. " Food Cost: " . $row["FoodCost"]. "<br>";   
   }
 } else {
   echo "0 results";
