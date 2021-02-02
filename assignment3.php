e3b77eb5705efee55e093da51cd9d5e34dbe2d59
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-02-02 14:09:44 -0700

    Select query update

--- assignment3.php
+++ assignment3.php
@@ -20,37 +20,16 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// insert statement
-//$sql = "INSERT INTO Food (FoodName, FoodCost)
-//VALUES ('taco', 9)";
-
-//Display records
-//$sql = "SELECT * FROM Food";
-
-// query table
-$sql = "SELECT FoodName, FoodCost FROM Food";
-// store query in variable
+$sql = "SELECT id, FoodName, FoodCost FROM Food";
 $result = $conn->query($sql);
 
-// If rows exist in result
-if ($result->num_rows > 0) 
-{
-    while($row = $result->fetch_assoc()) // put results in array and loop
-    {
-        echo "Food Name: " . $row["FoodName"]. "Food Cost: " . $row["FoodCost"]. "<br>";
-    }
-
-} else 
-{
-    echo "No results, something is wrong."
-}
-
-
-// record confirmation 
-if ($conn->query($sql) === TRUE) {
-  echo "Display";
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - FoodName: " . $row["FoodName"]. " - FoodCost" . $row["FoodCost"]. "<br>";
+  }
 } else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+  echo "0 results";
 }
 
 // close db connection
