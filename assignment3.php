35964c5e1c3add235c61972e8186bd23be7a1f02
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-02-02 13:41:51 -0700

    ..

--- assignment3.php
+++ assignment3.php
@@ -30,7 +30,7 @@ VALUES ('Lasagna', 8)";
 
 // record confirmation 
 if ($conn->query($sql) === TRUE) {
-  echo "New record 'Sandwich' created successfully";
+  echo "New record 'Lasagna' created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
