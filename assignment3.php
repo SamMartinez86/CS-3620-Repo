0e2637ef7263dfc9ffefb685d9dcaec09c38db95
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-02-02 14:16:33 -0700

    select *

--- assignment3.php
+++ assignment3.php
@@ -20,7 +20,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, FoodName, FoodCost FROM Food";
+$sql = "SELECT * FROM Food";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
