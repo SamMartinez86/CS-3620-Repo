0c226c1f90f4f738e54f44ddf71c6dd392760152
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-02-04 19:01:45 -0700

    asa

--- user.php
+++ user.php
@@ -1,14 +1,23 @@
 <?php
 
+require_once('./userDAO.php');
+
 CLass User
 {
+    private $username;
+
     public function getUser($user_id)
     {
+        
+    {
 
-    }
     public function getUsername()
     {
-        return "Sam";
+        return $username;
+    }
+    public function setUsername($username)
+    {
+        $this->username = $username;
     }
 }
 
