62cbabf74882e693f116c48c10d52b38c2b8b47a
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-02-09 17:58:24 -0700

    ...

---
+++ create_user.php
@@ -0,0 +1,19 @@
+<?php
+    require_once('./header.php');
+?>  
+
+<form method="POST" target="user_insert.php">
+
+    User ID:<input type="text" name="user_id" /><br />
+    Username:<input type="text" name="username" /><br />
+    First name:<input type="text" name="first_name" /><br />
+    Last name:<input type="text" name="last_name" /><br />
+    Password:<input type="text" name="password" /><br />
+
+    <input type="submit" value="Create User" />
+
+</form>
+
+<?php
+    require_once('./footer.php');
+?>  
