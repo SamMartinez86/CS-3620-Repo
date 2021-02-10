6d0b391d41598fcf15ef26e479afb123e0c7ad6b
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-02-09 18:53:57 -0700

    Update userDAO.php

--- user/userDAO.php
+++ user/userDAO.php
@@ -3,7 +3,7 @@ class UserDAO {
   function getUser($user){
     require_once('./utilities/connection.php');
     
-    $sql = "SELECT first_name, last_name, username, user_id FROM usertable WHERE user_id =" . $user->getUserId();
+    $sql = "SELECT * user_id FROM usertable " . $user->getUserId();
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
