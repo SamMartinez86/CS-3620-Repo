7cb4857c9e0232c0e6fe6f2e287860328068f987
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-02-11 18:07:39 -0700

    Create users.php

---
+++ users.php
@@ -0,0 +1,27 @@
+<?php
+
+    session_start();
+
+    require_once('./user/user.php');
+
+    $user = new user();
+    $user->getUser(2);
+
+    echo $user->getUsername();
+    echo "<br />";
+    echo $user->getFirstName();
+    echo "<br />";
+    echo $user->getLastName();
+
+    $user = new user();
+    $user->getUser($_GET["id"])
+
+    echo json_encode($user);
+
+    if ($result = $conn->query($sql) === TRUE) {
+        echo "Error";
+      } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+
+?>
