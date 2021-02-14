de5a0e057caf917a7c37d7a84af8bcf426dd6e1c
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-02-13 23:26:35 -0700

    ...

--- users.php
+++ users.php
@@ -10,11 +10,11 @@
     $user = new user();
     $user->getUser($_GET["id"]);
 
-    //echo json_encode($user);
+    echo json_encode($user);
 
     //$user = new user();
-    $user->getUserN($_GET["id"]);
+    //$user->getUserN($_GET["id"]);
 
-    echo json_encode($user);
+    //echo json_encode($user);
 
 ?>
