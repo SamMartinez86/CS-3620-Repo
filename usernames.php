5300c3681b723c888fc33c189a2e729a5ba75a09
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-02-13 23:34:22 -0700

    hopefully both will work this time

---
+++ usernames.php
@@ -0,0 +1,15 @@
+<?php
+   session_start();
+
+   ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+   require_once('./user/user.php');
+
+    $user = new user();
+    $user->getUserN($_GET["id"]);
+
+    echo json_encode($user);
+
+?>
