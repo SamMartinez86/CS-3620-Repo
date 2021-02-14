cfc092147ecc4caea6ecc3216e0407694b7886f0
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-02-13 23:24:01 -0700

    ...

--- users.php
+++ users.php
@@ -7,10 +7,10 @@
 
    require_once('./user/user.php');
 
-    //$user = new user();
-    //$user->getUser($_GET["id"]);
+    $user = new user();
+    $user->getUser($_GET["id"]);
 
-    //echo json_encode($user);
+    echo json_encode($user);
 
     $user = new user();
     $user->getUserN($_GET["id"]);
