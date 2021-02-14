b4f045850f9f15a3b80aac69d210b6ece4fe33b2
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-02-13 22:19:45 -0700

    mmm

--- user/userDAO.php
+++ user/userDAO.php
@@ -20,11 +20,10 @@ class UserDAO {
   }
 
   // for username
-  function getUserN(){
+  function getUserN($user){
     require_once('./utilities/connection.php');
     
-    //$sql = "SELECT first_name, last_name, username, user_id FROM user WHERE username =" . $user->getUsername();
-    $sql = "SELECT first_name, last_name, username, user_id FROM user WHERE username ='ironman@avengers.com'";
+    $sql = "SELECT first_name, last_name, username, user_id FROM user WHERE username = . $user->getUsername()";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
