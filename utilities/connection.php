3203e3e4c56797d8c5cef11cf48b5f5f6dad1bbe
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-02-23 18:57:45 -0700

    connection trouble

--- utilities/connection.php
+++ utilities/connection.php
@@ -1,10 +1,10 @@
 <?php
 $servername = "cs3620sqlsam.mysql.database.azure.com";
-$username = "sam@cs3620sqlsam";
+//$username = "sam@cs3620sqlsam";
 //$password = "1801Church";
 $username = (isset($_SESSION["SQLUSER"]) ? $_SESSION["SQLUSER"] : $_ENV['SQLUSER']);
 $password = (isset($_SESSION["SQLPW"]) ? $_SESSION["SQLPW"] : $_ENV['SQLPW']);
-//$dbname = "userschema";
+$dbname = "userschema";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
