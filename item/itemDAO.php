0d40b5e0098a04e16278688b3ee96d81da3a6a86
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-04-14 00:39:55 -0600

    item cost in search

--- item/itemDAO.php
+++ item/itemDAO.php
@@ -115,7 +115,8 @@ class itemDAO {
     require_once('./item/item.php');
 
     $sql = "SELECT item_id, item_name, item_description, item_cost, item_type, item_image, user_id FROM userschema.item WHERE item_name LIKE '%" . $search_keyword . "%'
-    OR item_type LIKE '%" . $search_keyword . "%'";
+    OR item_type LIKE '%" . $search_keyword . "%'
+    OR item_cost LIKE '%" . $search_keyword . "%'";
     $result = $conn->query($sql);
 
     $items = [];
