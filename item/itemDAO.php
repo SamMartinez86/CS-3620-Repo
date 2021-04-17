6f463279c08340674c3221ff4e0a311a0a7502d8
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-04-17 02:21:06 -0600

    getting close

--- item/itemDAO.php
+++ item/itemDAO.php
@@ -119,7 +119,7 @@ class itemDAO {
     $sql = "SELECT item_id, item_name, item_description, item_cost, item_type, item_image, user_id FROM userschema.item WHERE item_name LIKE '%" . $search_keyword . "%'
     OR item_type LIKE '%" . $search_keyword . "%'
     OR item_cost LIKE '%" . $search_keyword . "%'
-    ORDER BY '" . $order . "'";
+    ORDER BY " . $order;
     $result = $conn->query($sql);
 
     $items = [];
