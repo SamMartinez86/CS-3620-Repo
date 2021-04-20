c74689e9f67b193dd2d7a2858c657885ee1f9b23
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-04-20 13:01:20 -0600

    add item

--- addWishlist.php
+++ addWishlist.php
@@ -8,7 +8,7 @@ session_start();
 
 require_once('./wishList/wishList.php');
 require_once('./session/session.php');
-require_once('../item/item.php');
+require_once('./item/item.php');
 
 $wishlist = new wishList();
 $wishlist->setItemId($_POST["item_id"]);
