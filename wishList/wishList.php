d17fd9c1623f8d024db14726611cbe808c9bc2d3
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-04-20 10:15:35 -0600

    memory limit try 2

--- wishList/wishList.php
+++ wishList/wishList.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-ini_set(“memory_limit”,”16M“);
+ini_set('memory_limit','16M');
 
 class wishList implements \JsonSerializable {
   // Properties
