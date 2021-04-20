61a30718ba95615b8876cb15422f615f29379cad
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-04-20 10:31:19 -0600

    memory error try 4

--- addWishlist.php
+++ addWishlist.php
@@ -1,12 +1,12 @@
 <?php
 
-session_start();
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//require_once('./session/session.php');
+session_start();
+
+require_once('sessioncheck.php');
 
 require_once('./wishList/wishList.php');
 
--- wishList/wishListDAO.php
+++ wishList/wishListDAO.php
@@ -3,6 +3,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+require_once('./wishList/wishList.php');
 
 class wishListDAO {
 //    function getWishlist($wishlist){
