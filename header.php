de15378062796288c33598b9bf3dd5e6f71479b3
whitney smith <whitnaaays@gmail.com>
2021-04-22 14:38:41 -0600

    Changed header.php

--- header.php
+++ header.php
@@ -1,15 +1,24 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
 <style>
     h1 {
         text-align: center;
     }
-    .shiftl{
+
+    .shiftl {
         text-align: left;
     }
+
     a:link {
         text-decoration: none;
     }
@@ -52,12 +61,13 @@
             </li>
             -->
         </ul>
-<!--        <form class="form-inline my-2 my-lg-0" method='post'>-->
-<!--            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search_keyword">-->
-<!--            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>-->
-<!--        </form>-->
+        <!--        <form class="form-inline my-2 my-lg-0" method='post'>-->
+        <!--            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search_keyword">-->
+        <!--            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>-->
+        <!--        </form>-->
+        <div style="color: white; font-family: Monaco, sans-serif; margin-right: 25px; font-size: 25px;">Electronics Marketplace</div>
         <form class="form-inline my-2 my-lg-0" method='post' style="margin-left: 10px;">
-            <input  class="btn btn-outline-light my-2 my-sm-0" type="submit" value="Reset" name="reset" />
+            <input class="btn btn-outline-light my-2 my-sm-0" type="submit" value="Reset" name="reset"/>
         </form>
     </div>
 </nav>
